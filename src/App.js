499645d82b955d3070be36d85771ff0e3159b835
root <root@MG-LAPTOP.>
2025-08-02 17:42:07 +0200

    Changed app text to confirm

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Github Actions test.
+          Github Actions test AFTER fixes.
           <br></br>
           Edit <code>src/App.js</code> and save to reload.
         </p>
