2095cf4de2d37e32647b2dc3058ea9ffec10ce4f
root <root@MG-LAPTOP.>
2025-08-02 20:50:26 +0200

    Changed page text to confirm working deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Github Actions test AFTER fixes.
+          Github Actions test after ALL fixes - confirmed.
           <br></br>
           Edit <code>src/App.js</code> and save to reload.
         </p>
